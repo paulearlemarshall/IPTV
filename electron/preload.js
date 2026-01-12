91368426ce60d4b5024ecda8caab0c0349a76a05
Paul Marshall <paul.e.marshall@gmail.com>
2026-01-12 20:18:54 +0400

    Cleanup redundant download progress IPC methods in preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -15,9 +15,7 @@ contextBridge.exposeInMainWorld('api', {
   cleanupProfileImages: (data) => ipcRenderer.invoke('cleanup-profile-images', data),
   onCastDeviceFound: (callback) => ipcRenderer.on('cast-device-found', (event, name) => callback(name)),
   onM3UData: (callback) => ipcRenderer.on('m3u-batch', (event, data) => callback(data)),
-  onProgress: (callback) => ipcRenderer.on('download-progress', (event, data) => callback(data)),
   onDownloadLog: (callback) => ipcRenderer.on('download-log', (event, msg) => callback(msg)),
-  removeProgressListeners: () => ipcRenderer.removeAllListeners('download-progress'),
   config: {
       load: () => ipcRenderer.invoke('get-config'),
       save: (data) => ipcRenderer.invoke('save-config', data)
